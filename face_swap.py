cebdef81dc66fb7da3e95486b8b50aed96912468
Heo-Jung <67816598+Heo-Jung@users.noreply.github.com>
2022-04-21 14:55:44 +0900

    main 파일 생성 1. 선택 기다림 2. face_swap 3. make_mask 4. filter
    모드 나오기는 ESC!

--- face_swap.py
+++ face_swap.py
@@ -15,65 +15,71 @@ def white_balance(_frame):
     return img_yuv
 
 
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
-cap = cv.VideoCapture(0 + cv.CAP_DSHOW)
-#### debug
-if DEBUG:
-    img_frame = cv.imread("people.jpg")
-
-while True:
-    #### debug
-    if not DEBUG:
-        ret, img_frame = cap.read()
-    img_rst = img_frame.copy() # 출력 용 이미지
-    img_copy = img_frame.copy() # 원본 이미지 복사 -> img_copy
-
-    img_gray = cv.cvtColor(white_balance(img_frame), cv.COLOR_BGR2GRAY)
-    dets = detector(img_gray, 1)
-    mask, rois, result = [], [], []
-
-    for face in dets:
-        shape = predictor(img_frame, face)  # 얼굴에서 68개 점 찾기
-        list_points = []
-        _mask = np.zeros(img_frame.shape, np.uint8)
-        for p in shape.parts(): # 점 68개 리스트
-            list_points.append([p.x, p.y])
-
-        list_points = np.array(list_points)
-
-        pt = list(range(0, 27))
-        pt[17:27] = pt[26:16:-1]
-        mask_pt = []
-        for x in pt:
-            mask_pt.append([list_points[x][0], list_points[x][1]])
-
-        # 제로 이미지에 얼굴 위치를 잡아 백색 다각형을 생성 -> 마스킹용
-        _mask = cv.fillPoly(cv.polylines(_mask, [np.int32(mask_pt)], True, (255, 255, 255)), [np.int32(mask_pt)], (255, 255, 255))
-        _masked = _mask[face.top(): face.bottom(), face.left(): face.right()]
-        mask.append(_masked.copy())
-
-        rois.append(img_rst[face.top(): face.bottom(), face.left(): face.right()]) # rois에 인식된 얼굴들의 각 RoI를 저장
-        result.append(img_copy[face.top(): face.bottom(), face.left(): face.right()]) # 얼굴 합성할 베이스
+def face_swap(cam):
     #### debug
     if DEBUG:
-        for i in range(len(rois)):
-            cv.imshow('rois'+str(i), rois[i])
-            cv.imshow("mask"+str(i), mask[i])
+        img_frame = cv.imread("people.jpg")
 
-    if len(rois) > 1: # 얼굴이 둘 이상 인식되었을 때
-        for i in range(len(rois)):
-            j = i + 1
-            if i == len(rois)-1:
-                j = 0
-            temp = rois[i].copy()
-            temp = cv.resize(temp, dsize=(rois[j].shape[1], rois[j].shape[0]))
-            _mask = mask[i].copy()
-            _mask = cv.resize(_mask, dsize=(rois[j].shape[1], rois[j].shape[0]))
-            cv.copyTo(temp, _mask, result[j])
-    cv.imshow('result', img_copy)
-    key = cv.waitKey(30)
-    if key == 27:
-        break
+    while True:
+        #### debug
+        if not DEBUG:
+            ret, img_frame = cap.read()
+        img_rst = img_frame.copy()  # 출력 용 이미지
+        img_copy = img_frame.copy()  # 원본 이미지 복사 -> img_copy
 
-cap.release()
+        img_gray = cv.cvtColor(white_balance(img_frame), cv.COLOR_BGR2GRAY)
+        dets = detector(img_gray, 1)
+        mask, rois, result = [], [], []
+
+        for face in dets:
+            shape = predictor(img_frame, face)  # 얼굴에서 68개 점 찾기
+            list_points = []
+            _mask = np.zeros(img_frame.shape, np.uint8)
+            for p in shape.parts():  # 점 68개 리스트
+                list_points.append([p.x, p.y])
+
+            list_points = np.array(list_points)
+
+            pt = list(range(0, 27))
+            pt[17:27] = pt[26:16:-1]  # 얼굴 윤곽선 점
+            mask_pt = []
+            for x in pt:
+                mask_pt.append([list_points[x][0], list_points[x][1]])
+
+            # 제로 이미지에 얼굴 위치를 잡아 백색 다각형을 생성 -> 마스킹용
+            _mask = cv.fillPoly(cv.polylines(_mask, [np.int32(mask_pt)], True, (255, 255, 255)), [np.int32(mask_pt)],
+                                (255, 255, 255))
+            _masked = _mask[face.top(): face.bottom(), face.left(): face.right()]
+            mask.append(_masked.copy())
+
+            rois.append(img_rst[face.top(): face.bottom(), face.left(): face.right()])  # rois에 인식된 얼굴들의 각 RoI를 저장
+            result.append(img_copy[face.top(): face.bottom(), face.left(): face.right()])  # 얼굴 합성할 베이스
+        #### debug
+        if DEBUG:
+            for i in range(len(rois)):
+                cv.imshow('rois' + str(i), rois[i])
+                cv.imshow("mask" + str(i), mask[i])
+
+        if len(rois) > 1:  # 얼굴이 둘 이상 인식되었을 때
+            for i in range(len(rois)):
+                j = i + 1
+                if i == len(rois) - 1:
+                    j = 0
+                temp = rois[i].copy()
+                temp = cv.resize(temp, dsize=(rois[j].shape[1], rois[j].shape[0]))
+                _mask = mask[i].copy()
+                _mask = cv.resize(_mask, dsize=(rois[j].shape[1], rois[j].shape[0]))
+                cv.copyTo(temp, _mask, result[j])
+        cv.imshow('result', img_copy)
+        key = cv.waitKey(1)
+        if key == 27:
+            break
+    cam.release()
+
+
+if __name__ == "__main__":
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+    cap = cv.VideoCapture(0 + cv.CAP_DSHOW)
+
+    face_swap(cap)
--- make_mask.py
+++ make_mask.py
@@ -2,89 +2,42 @@ import dlib
 import cv2 as cv
 import numpy as np
 
-detector = dlib.get_frontal_face_detector()
+white_color = (255, 255, 255)
 
-predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+def make_mask(cam):
+    while True:
+        ret, img_frame = cam.read()
+        h, w = img_frame.shape[:2]
+        img_gray = cv.cvtColor(img_frame, cv.COLOR_BGR2GRAY)
+        dets = detector(img_gray, 1)
 
-cap = cv.VideoCapture(0+cv.CAP_DSHOW)
+        for face in dets:
+            shape = predictor(img_frame, face)  # 얼굴에서 68개 점 찾기
+            list_points = []
+            #포인트 좌표
+            for p in shape.parts():
+                list_points.append([p.x, p.y])
+            list_points = np.array(list_points)
 
-# range는 끝값이 포함안됨
-ALL = list(range(0, 68))
-RIGHT_EYEBROW = list(range(17, 22))
-LEFT_EYEBROW = list(range(22, 27))
-RIGHT_EYE = list(range(36, 42))
-LEFT_EYE = list(range(42, 48))
-NOSE = list(range(27, 36))
-MOUTH_OUTLINE = list(range(48, 61))
-MOUTH_INNER = list(range(61, 68))
-JAWLINE = list(range(0, 17))
-TEST = list([2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 28])
-test = np.array([28, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2], np.int32)
-index = ALL
-white_color=(255,255,255)
-while True:
+            pt = list(range(2, 15))  # 마스크 위치 점
+            pt.append(28)
+            pt[:] = pt[::-1]
+            mask_pt = []
+            for x in pt:
+                mask_pt.append([list_points[x][0], list_points[x][1]])
 
-    ret, img_frame = cap.read()
-    h,w,c=img_frame.shape
-    img = cv.imread('Test.jpg')
-    img = cv.resize(img, (w, h), interpolation=cv.INTER_AREA)
-    img_gray = cv.cvtColor(img_frame, cv.COLOR_BGR2GRAY)
-    dets = detector(img_gray, 1)
+            img_frame = cv.fillConvexPoly(img_frame, np.array([mask_pt], np.int32), white_color)
+        cv.flip(img_frame, 1, img_frame)
+        cv.imshow('result', img_frame)
+        key = cv.waitKey(1)
+        if key == 27:
+            break
+    cam.release()
 
-    for face in dets:
-        shape = predictor(img_frame, face)  # 얼굴에서 68개 점 찾기
-        list_points = []
-        #포인트 좌표
-        for p in shape.parts():
-            list_points.append([p.x, p.y])
-        list_points = np.array(list_points)
 
-        img_frame= cv.fillConvexPoly(img_frame, np.array([[list_points[28][0], list_points[28][1]], [list_points[14][0], list_points[14][1]], [list_points[13][0], list_points[13][1]],
-                                                          [list_points[12][0], list_points[12][1]], [list_points[11][0], list_points[11][1]], [list_points[10][0], list_points[10][1]],
-                                                          [list_points[9][0], list_points[9][1]], [list_points[8][0], list_points[8][1]], [list_points[7][0], list_points[7][1]],
-                                                          [list_points[6][0], list_points[6][1]], [list_points[5][0], list_points[5][1]], [list_points[4][0], list_points[4][1]],
-                                                          [list_points[3][0], list_points[3][1]], [list_points[2][0], list_points[2][1]]], np.int32), white_color)
-        #얼굴 마스크 생성과 원하는 이미지에 copyTo해서 씌우기
-        '''
-        mask = np.zeros(img_frame.shape, dtype=np.uint8)
-        roi_corners = np.int32([np.array(
-            [np.array(
-                [[list_points[14][0], list_points[14][1]], [list_points[13][0], list_points[13][1]],
-                 [list_points[12][0], list_points[12][1]],
-                 [list_points[11][0], list_points[11][1]], [list_points[10][0], list_points[10][1]],
-                 [list_points[9][0], list_points[9][1]],
-                 [list_points[8][0], list_points[8][1]], [list_points[7][0], list_points[7][1]],
-                 [list_points[6][0], list_points[6][1]],
-                 [list_points[5][0], list_points[5][1]], [list_points[4][0], list_points[4][1]],
-                 [list_points[3][0], list_points[3][1]],
-                 [list_points[2][0], list_points[2][1]], [list_points[3][0], list_points[3][1]],
-                 [list_points[2][0], list_points[2][1]],
-                 [list_points[1][0], list_points[1][1]], [list_points[0][0], list_points[0][1] - 50],
-                 [list_points[17][0], list_points[17][1] - 50],
-                 [list_points[18][0], list_points[18][1] - 50], [list_points[19][0], list_points[19][1] - 60],
-                 [list_points[20][0], list_points[20][1] - 70],
-                 [list_points[21][0], list_points[21][1] - 80], [list_points[22][0], list_points[22][1] - 80],
-                 [list_points[23][0], list_points[23][1] - 70],
-                 [list_points[24][0], list_points[24][1] - 60], [list_points[25][0], list_points[25][1] - 50],
-                 [list_points[26][0], list_points[26][1] - 50],
-                 [list_points[16][0], list_points[16][1] - 50], [list_points[15][0], list_points[15][1]]])])])
-        channel_count = img_frame.shape[2]
-        ignore_mask_color = (255,) * channel_count
-        cv.fillPoly(mask, roi_corners, ignore_mask_color)
+if __name__ == "__main__":
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+    cap = cv.VideoCapture(0 + cv.CAP_DSHOW)
 
-        cv.copyTo(img_frame, mask, img)
-        masked_image = cv.bitwise_and(img_frame, mask)
-
-        cv.imshow("test", masked_image)
-        #cv.flip(img, 1, img)
-        '''
-    #cv.imshow("img", img)
-    cv.flip(img_frame,1,img_frame)
-    cv.imshow('result', img_frame)
-    key = cv.waitKey(1)
-
-    if key == 27:
-        break
-
-cap.release()
-
+    make_mask(cap)
